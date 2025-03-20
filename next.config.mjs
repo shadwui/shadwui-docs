bf520d7b17b113fa6204a6b03459edcbf26b4ab2
sahilkumardev <sahilkumar710@outlook.com>
2025-03-20 14:32:29 +0530

    feat: update Next.js configuration to ignore ESLint during builds and modify webpack rules; comment out global CSS import in layout component

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.d\.ts$/,
+      exclude: /node_modules/,
+      use: [{ loader: 'ignore-loader' }]
+    });
+    
+    return config;
+  },
 };
 
 export default withMDX(config);
--- registry/src/auth/code/mongodb/components/layout.tsx
+++ registry/src/auth/code/mongodb/components/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 import { Toaster } from "@/registry/src/auth/code/mongodb/components/ui/sonner";
 import AuthProvider from "@/registry/src/auth/code/mongodb/context/auth-provider";
  
